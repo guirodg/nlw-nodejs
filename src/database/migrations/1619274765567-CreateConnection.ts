9b0cc40ccac994e44ff9bcde5113800e2bb1f282
guirodg <guizinhord@hotmail.com>
2021-04-24 12:46:47 -0300

    feat: connection websocket

---
+++ src/database/migrations/1619274765567-CreateConnection.ts
@@ -0,0 +1,61 @@
+import {MigrationInterface, QueryRunner, Table, TableForeignKey} from "typeorm";
+
+export class CreateConnection1619274765567 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "connections",
+                columns: [
+                    {
+                        name: "id",
+                        type: "varchar",
+                        isPrimary: true,
+                    },
+                    {
+                        name: "admin_id",
+                        type: "varchar",
+                        isNullable: true,
+                    },
+                    {
+                        name: "user_id",
+                        type: "varchar",
+                        isNullable: true,
+                    },
+                    {
+                        name: "socket_id",
+                        type: "varchar"
+                    },
+                    {
+                        name: "create_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                    {
+                        name: "update_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        );
+
+        await queryRunner.createForeignKey(
+            "connections",
+            new TableForeignKey({
+                name: "FKConnectionUser",
+                referencedTableName: "users",
+                referencedColumnNames: ["id"],
+                columnNames: ["user_id"],
+                onDelete: "SET NULL",
+                onUpdate: "CASCADE",
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey("connections", "FKConnectionUser");
+        await queryRunner.dropTable("connections")
+    }
+
+}
